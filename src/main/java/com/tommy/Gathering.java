38b7ea4d9583a6c071b923e6b31e0df2be42cfdd
LimHanGyeol <dlagksruf19@naver.com>
2020-12-14 19:21:15 +0900

    [바이트코드 조작] 코드 커버리지는 어떻게 측정할까?

---
+++ src/main/java/com/tommy/Gathering.java
@@ -0,0 +1,27 @@
+package com.tommy;
+
+public class Gathering {
+
+    /**
+     * 최대 참가자
+     */
+    private final int maxNumberOfAttendees;
+
+    /**
+     * 현재 신청한 인원
+     */
+    private final int numberOfEnrollment;
+
+    public Gathering(int maxNumberOfAttendees, int numberOfEnrollment) {
+        this.maxNumberOfAttendees = maxNumberOfAttendees;
+        this.numberOfEnrollment = numberOfEnrollment;
+    }
+
+    public boolean isEnrollmentFull() {
+        if (maxNumberOfAttendees == 0) {
+            return false;
+        }
+        return numberOfEnrollment >= maxNumberOfAttendees;
+    }
+
+}
---
+++ src/test/java/com/tommy/GatheringTest.java
@@ -0,0 +1,38 @@
+package com.tommy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GatheringTest {
+
+    @Test
+    @DisplayName("모임인원이 모두 차지 않았을 경우")
+    void gatheringIsEnrollmentFullIsFalse() {
+        Gathering gathering = new Gathering(100, 10);
+        assertThat(gathering.isEnrollmentFull()).isFalse();
+    }
+
+    @Test
+    @DisplayName("모임인원이 모두 찼을 경우")
+    void gatheringIsEnrollmentFullIsTrue() {
+        Gathering gathering = new Gathering(100, 100);
+        assertThat(gathering.isEnrollmentFull()).isTrue();
+    }
+
+    @Test
+    @DisplayName("모임인원이 초과했을 경우")
+    void gatheringIsEnrollmentFullIsExceed() {
+        Gathering gathering = new Gathering(100, 110);
+        assertThat(gathering.isEnrollmentFull()).isTrue();
+    }
+
+    @Test
+    @DisplayName("모임인원의 최대 인원이 0일 경우")
+    void gatheringIsMaxNumberOfAttendeesIsZero() {
+        Gathering gathering = new Gathering(0, 10);
+        assertThat(gathering.isEnrollmentFull()).isFalse();
+    }
+
+}
